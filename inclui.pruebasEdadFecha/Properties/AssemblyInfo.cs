087285792f35159b994800924eb3935e96345668
airam hernandez hernandez <airamhh@hotmail.com>
2022-03-27 18:37:31 +0200

    27/03/2022 Airam - Solucionado el error que no permitía añadir las pruebas unitarias al repositorio remoto.

---
+++ inclui.pruebasEdadFecha/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("inclui.pruebasEdadFecha")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("inclui.pruebasEdadFecha")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fe92cdc0-723c-45c3-aa58-b5562208ddf0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ inclui.pruebasEdadFecha/PruebasEdadFecha.cs
@@ -0,0 +1,175 @@
+﻿using inclui.edadfecha;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace inclui.pruebasEdadFecha
+{
+    [TestClass]
+    public class PruebasEdadFecha
+    {
+        [TestMethod]
+        public void HallarDiferenciaEnAnioFechaACyFechaDC()
+        {
+            DatosAnio.InformacionAnio primeraFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("01/02/2001"),
+                antesCristo = true
+            };
+
+            DatosAnio.InformacionAnio segundaFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("02/12/2023"),
+                antesCristo = false
+            };
+
+            int resultadoReal = CSComprobaciones.DevolverDiferenciaAnios(primeraFecha, segundaFecha);
+            int resultadoEsperado = 4024;
+
+            Assert.AreEqual(resultadoEsperado, resultadoReal);
+        }
+
+        [TestMethod]
+        public void HallarDiferenciaEnDiasFechaACyFechaDC()
+        {
+            DatosAnio.InformacionAnio primeraFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("01/02/2001"),
+                antesCristo = true
+            };
+
+            DatosAnio.InformacionAnio segundaFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("02/12/2023"),
+                antesCristo = false
+            };
+
+            int resultadoReal = CSComprobaciones.DevolverDiferenciaDias(primeraFecha, segundaFecha);
+            int resultadoEsperado = 1469371;
+
+            Assert.AreEqual(resultadoEsperado, resultadoReal);
+        }
+        [TestMethod]
+        public void DiferenciasAnioBisiesto()
+        {
+            DatosAnio.InformacionAnio primeraFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("29/2/2000"),
+                antesCristo = false
+            };
+
+            DatosAnio.InformacionAnio segundaFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("28/2/2001"),
+                antesCristo = false
+            };
+
+            int resultadoReal = CSComprobaciones.DevolverDiferenciaAnios(primeraFecha, segundaFecha);
+            int resultadoEsperado = 1;
+
+            Assert.AreEqual(resultadoEsperado, resultadoReal);
+        }
+
+        [TestMethod]
+        public void DiferenciasFebreroMarzoBisiesto()
+        {
+            DatosAnio.InformacionAnio primeraFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("29/2/2000"),
+                antesCristo = false
+            };
+
+            DatosAnio.InformacionAnio segundaFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("1/3/2003"),
+                antesCristo = false
+            };
+
+            int resultadoReal = CSComprobaciones.DevolverDiferenciaAnios(primeraFecha, segundaFecha);
+            int resultadoEsperado = 3;
+
+            Assert.AreEqual(resultadoEsperado, resultadoReal);
+        }
+
+        [TestMethod]
+        public void DiferenciasAnioBisiesto2()
+        {
+            DatosAnio.InformacionAnio primeraFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("29/2/2000"),
+                antesCristo = false
+            };
+
+            DatosAnio.InformacionAnio segundaFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("29/2/2004"),
+                antesCristo = false
+            };
+
+            int resultadoReal = CSComprobaciones.DevolverDiferenciaAnios(primeraFecha, segundaFecha);
+            int resultadoEsperado = 4;
+
+            Assert.AreEqual(resultadoEsperado, resultadoReal);
+        }
+        [TestMethod]
+        public void DiferenciasAnioBisiesto3()
+        {
+            DatosAnio.InformacionAnio primeraFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("28/2/2001"),
+                antesCristo = false
+            };
+
+            DatosAnio.InformacionAnio segundaFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("29/2/2004"),
+                antesCristo = false
+            };
+
+            int resultadoReal = CSComprobaciones.DevolverDiferenciaAnios(primeraFecha, segundaFecha);
+            int resultadoEsperado = 3;
+
+            Assert.AreEqual(resultadoEsperado, resultadoReal);
+        }
+
+        [TestMethod]
+        public void DiferenciasAnioBisiesto4()
+        {
+            DatosAnio.InformacionAnio primeraFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("29/2/2000"),
+                antesCristo = false
+            };
+
+            DatosAnio.InformacionAnio segundaFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("28/2/2003"),
+                antesCristo = false
+            };
+
+            int resultadoReal = CSComprobaciones.DevolverDiferenciaAnios(primeraFecha, segundaFecha);
+            int resultadoEsperado = 3;
+
+            Assert.AreEqual(resultadoEsperado, resultadoReal);
+        }
+        [TestMethod]
+        public void DiferenciasAnioBisiestoAntesDeCristo()
+        {
+            DatosAnio.InformacionAnio primeraFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("1/5/2000"),
+                antesCristo = false
+            };
+
+            DatosAnio.InformacionAnio segundaFecha = new DatosAnio.InformacionAnio
+            {
+                fecha = DateTime.Parse("12/4/2003"),
+                antesCristo = true
+            };
+
+            int resultadoReal = CSComprobaciones.DevolverDiferenciaAnios(primeraFecha, segundaFecha);
+            int resultadoEsperado = 4008;
+
+            Assert.AreEqual(resultadoEsperado, resultadoReal);
+        }
+    }
+}
